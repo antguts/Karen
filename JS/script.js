594c308758fba00f950bc0464e82743cabfcaa34
antguts <antgut035@gmail.com>
2020-06-22 18:04:42 -0400

    Added level change

--- JS/script.js
+++ JS/script.js
@@ -5,32 +5,30 @@ let audioDiv=document.querySelector('.audio')
 let audioTag=document.querySelector('audio')
 let vol= () =>{document.querySelector('audio').volume=0.4}
 
-// window.onload = (event) => {
-    let startBtn=document.querySelector('button')
-    // audioDiv.innerHTML=`<audio controls loop autoplay> 
-    // <source src="assets/sounds/menuScreen2.mp3" type="audio/mpeg">
-    // </audio>`
+audioDiv.innerHTML=`<audio controls loop autoplay> 
+<source src="assets/sounds/menuScreen2.mp3" type="audio/mpeg">
+</audio>`
+vol()
+let startBtn=document.querySelector('button')
+startBtn.onclick = () =>{
+    // console.log(karens[i].img)
+    populateKarens()
+    animate()
+    audioDiv.innerHTML=`<audio controls loop autoplay><source src="assets/sounds/zombieWav.mp3" 
+    type="audio/mpeg"></audio>`
     vol()
-    startBtn.onclick = () =>{
-        // audioDiv.innerHTML=`<audio controls loop autoplay><source src="assets/sounds/zombieWav.mp3" 
-        // type="audio/mpeg"></audio>`
-        populateKarens()
-        // console.log(karens[i].img)
-        animate()
-        vol()
-    } 
+} 
     
-// };
 
 
 
 //KARENS=======================================================================================
 const karens=[]
 class Karen {
-    constructor(x,img){
+    constructor(x,img,width){
         this.y=200, //Same TOP CSS position for all.
         this.x=x //To be determined by random assign. Between 60-517
-        this.width=75, 
+        this.width=width, 
         this.hp=2, //Hit Points for each Karen
         this.img=img //Image for Karen
     }
@@ -47,56 +45,56 @@ let bossKaren={
 
 function populateKarens(){
     //10 Karens...
-    for(i=0;i<10;i++){
-        // let kar=document.querySelector('.container')
+    for(i=0;i<3;i++){
         let ranIm='zom'+Math.floor((Math.random()*2)+1)+'.png'
-        let ranX=Math.floor((Math.random()*635)+20)
-        let karen=new Karen(ranX,ranIm)
-        // console.log(karen.width)
+        let ranX=Math.floor((Math.random()*635)+10)
+        let ranWidth=Math.floor((Math.random()*50)+135)
+        let karen=new Karen(ranX,ranIm,ranWidth)
         karens.push(karen)
-        // kar.innerHTML+=`<img src="/assets/${ranIm}" class="karen" id="karen${i}" style="width:105px; margin-left:${ranX}px;" onclick='karenShot(this)'>`
-        // let kar = new Karen(ranX,ranIm)
-        // karens.push(kar)
     }
 }//end funct
 
-//Function to shoot and eliminate Karens
+//Function to shoot and eliminate Karens. Counter used to track the dead instead of array methods to maintain properties.
+let count=0
 function karenShot(element){
+    gsap.to(`#${element.id}`,{opacity: 1, width:45})
+    console.log(count)
     new Audio('/assets/sounds/shot.wav').play();
     //grab number from id and eliminate the 'karen'
     var currKaren = element.id.match(/\d/g).join('');
     karens[currKaren].hp--
-    console.log(karens[currKaren].hp)
     //checks HP of Karen
     if(karens[currKaren].hp<1){
-        element.style.display='none'
+        count++
+        element.remove()
+        if(count==karens.length) levelChange()
     }
-}
+}//end karen shot
 
 
 //ANIMATION=========================================================================================
 
-// kar.innerHTML+=`<img src="/assets/${ranIm}" class="karen" id="karen${i}" style="width:105px; margin-left:${ranX}px;" onclick='karenShot(this)'>`
 
-//function to set a delay
+//function to set a delay on Zombie Spawn
 function delay(i) { 
     setTimeout(function() { 
-        container.innerHTML+=`<img src="/assets/${karens[i].img}" class="karen" id="karen${i}" style="width:${karens[i].width}px; margin-left:${karens[i].x}px;" onclick='karenShot(this)'>`
+        container.innerHTML+=`<img src="/assets/${karens[i].img}" class="karen" id="karen${i}" style="width:${karens[i].width}px; margin-left:${karens[i].x}px;" onclick='karenShot(this)'>`;
     }, 1500 * i); 
-  }
+}//end delay
 
 function animate(){
     for(i=0;i<karens.length;i++){
-            delay(i)
-        
-        if(karens.length==i) return console.log("finished")
-    }
-     
+        delay(i)
+    }     
+}//end animate
 
 
-    // console.log(karens[i].width)
-    // container.innerHTML+=`<img src="${karens[i]}" class="karen" id="karen${i}" style="width:${karens[i].width}; margin-left:${karens[i].x}px;" onclick='karenShot(this)'> `
-
-    // populateKarens()
+function levelChange(){
+    container.innerHTML+=`<h3>The park darkens as you hear a loud shriek....</h3>`
+    var tl=gsap.timeline({defaults:{duration: 1}})
+    tl.to('.square',{opacity: 1, duration: 5})
+      .to('h3',{opacity: 1, duration: 6},"-=2.2")
+      .to(".container",{backgroundImage:'url(assets/FinalBossHell.jpg)'})
+      .to('h3',{opacity: 0, duration: 5})
+      .to('.square',{opacity: 0, duration: 8},"-=2.2")
 }
-
