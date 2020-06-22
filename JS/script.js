d33f78182439c43db0e1e44bb032e9c480aa8435
antguts <antgut035@gmail.com>
2020-06-22 12:04:10 -0400

    Updated sounds

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,16 @@
 //AUDIO ELEMENTS===========================================================================
+let audioDiv=document.querySelector('.audio')
+let audioTag=document.querySelector('audio')
 
-document.querySelector('audio').volume=0.5
+window.onload = (event) => {
+    // audioDiv.innerHTML=`<audio controls loop autoplay> 
+    // <source src="assets/sounds/menuScreen2.mp3" type="audio/mpeg">
+    // </audio>`
+    // audioTag.volume=0.2
+};
 
-//Change music by changing the inner HTML of audio. Simple way.
 document.querySelector('button').onclick = () =>{
-    let aud=document.querySelector('.audio')
-    aud.innerHTML=`<audio controls loop autoplay><source src="assets/sounds/finalBoss.mp3" 
+    audioDiv.innerHTML=`<audio controls loop autoplay><source src="assets/sounds/zombieWav.mp3" 
     type="audio/mpeg"></audio>`
-    document.querySelector('audio').volume=0.5
+    // audioTag.volume=0.6
 }
